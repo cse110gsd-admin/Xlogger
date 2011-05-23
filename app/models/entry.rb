7fde33d2b7e62348397800107de2560c834c909f
Will <wjdhollow@gmail.com>
2011-05-22 17:06:30 -0700

    Changed associations from has_one to has_many

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,6 @@ class Entry < ActiveRecord::Base
 
   belongs_to :user
 
-  has_one  :workout, :dependent =>  :destroy
-  has_one  :warmup,  :dependent =>  :destroy
+  has_many  :workouts, :dependent =>  :destroy
+  has_many  :warmups,  :dependent =>  :destroy
 end
