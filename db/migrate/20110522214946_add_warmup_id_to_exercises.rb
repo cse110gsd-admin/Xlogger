d4f85e67213cec2bbc5a55c31aa4912f47a304eb
Jake Rabinowitz <jrabinow@ucsd.edu>
2011-05-23 16:26:26 -0700

    routes

--- db/migrate/20110522214946_add_warmup_id_to_exercises.rb
+++
@@ -1,9 +0,0 @@
-class AddWarmupIdToExercises < ActiveRecord::Migration
-  def self.up
-    add_column :exercises, :warmup_id, :integer
-  end
-
-  def self.down
-    remove_column :exercises, :warmup_id
-  end
-end
--- db/migrate/20110523002328_add_user_id_to_workouts.rb
+++
@@ -1,9 +0,0 @@
-class AddUserIdToWorkouts < ActiveRecord::Migration
-  def self.up
-    add_column :workouts, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :workouts, :user_id
-  end
-end
