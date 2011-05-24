39d55e34394b990b1d9d68301fdf5e873df7b304
cse110gsd-admin <jrabinow@ucsd.edu>
2011-05-23 17:58:24 -0700

    git is lame

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-<<<<<<< HEAD
     @title = "Home"
   end
 
@@ -14,11 +13,10 @@ class PagesController < ApplicationController
 
   def help
     @title = "Help"
-=======
   end
 
   def contact
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
+
   end
 
 end
