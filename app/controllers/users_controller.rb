94abebaa60d467da3527bfe155738684838d8fe2
cse110gsd-admin <jrabinow@ucsd.edu>
2011-05-23 18:01:59 -0700

    git is lame

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
   before_filter :authenticate, :only => [:edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update, :destroy]
   # "Edit" and "Account Management" are the same.
-
-
-=======
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
   # GET /users
   # GET /users.xml
   def index
@@ -22,11 +17,7 @@ class UsersController < ApplicationController
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-<<<<<<< HEAD
     @title = @user.name
-=======
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
@@ -36,7 +27,6 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.xml
   def new
-<<<<<<< HEAD
     @title = "Sign up"
     @user = User.new
 
@@ -44,40 +34,32 @@ class UsersController < ApplicationController
    #   format.html # new.html.erb
    #   format.xml  { render :xml => @user }
    # end
-=======
     @user = User.new
 
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user }
     end
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
   end
 
   # GET /users/1/edit
   def edit
-<<<<<<< HEAD
     @title = "Edit user"
-=======
     @user = User.find(params[:id])
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
   end
 
   # POST /users
   # POST /users.xml
   def create
     @user = User.new(params[:user])
-<<<<<<< HEAD
     respond_to do |format|
       if @user.save
         sign_in @user
         format.html { redirect_to(@user, :notice => 'Registration successful!') }
-=======
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
@@ -93,16 +75,13 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-<<<<<<< HEAD
         format.html { redirect_to(@user, :notice => 'Account info updated.') }
         format.xml  { head :ok }
       else
         @title = "Edit user"
-=======
         format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
         format.xml  { head :ok }
       else
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
         format.html { render :action => "edit" }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
@@ -116,7 +95,6 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-<<<<<<< HEAD
       format.html { redirect_to(root_path, :notice => 'Account deleted.') }
       format.xml  { head :ok }
     end
@@ -135,10 +113,8 @@ class UsersController < ApplicationController
     end
 
 
-=======
       format.html { redirect_to(users_url) }
       format.xml  { head :ok }
     end
   end
->>>>>>> 26757e2a401091b80e8352c264b191333628db9b
 end
